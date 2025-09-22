db86dd7c266b38657f28b8ea85f507b680637c05
LuMarans30 <andreamarano2004@gmail.com>
2025-09-23 00:22:35 +0200

    Add http server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-use esp_idf_hal::{delay::FreeRtos, prelude::*};
-use esp_idf_svc::{eventloop::EspSystemEventLoop, log::*, sys::*};
+use esp_idf_hal::{
+    delay::FreeRtos,
+    io::{EspIOError, Write},
+    prelude::*,
+};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    http::{
+        server::{Configuration, EspHttpServer},
+        Method,
+    },
+    log::*,
+    sys::*,
+};
 
 use crate::util::wifi;
 
@@ -48,7 +60,42 @@ fn main() -> Result<()> {
         }
     };
 
+    let mut server = EspHttpServer::new(&Configuration::default())?;
+
+    server.fn_handler(
+        "/",
+        Method::Get,
+        |request| -> core::result::Result<(), EspIOError> {
+            let html = index_html();
+            let mut response = request.into_ok_response()?;
+            response.write_all(html.as_bytes())?;
+            Ok(())
+        },
+    )?;
+
     loop {
         FreeRtos::delay_ms(1000);
     }
+}
+
+fn templated(content: impl AsRef<str>) -> String {
+    format!(
+        r#"
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>esp-rs web server</title>
+    </head>
+    <body>
+        {}
+    </body>
+</html>
+"#,
+        content.as_ref()
+    )
+}
+
+fn index_html() -> String {
+    templated("Hello from ESP32 Open Variometer!")
 }
